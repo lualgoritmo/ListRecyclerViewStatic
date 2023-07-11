65ebd965bdd53c828b52ce6d817e02d8938f7c25
lualgoritmo <lualgoritmo@outlook.com>
2023-07-10 21:43:47 -0300

    Criar a class da custom view

--- CustomView/app/src/main/java/com/example/customview/ui/custom/ItemSection.kt
+++ CustomView/app/src/main/java/com/example/customview/ui/custom/ItemSection.kt
@@ -2,14 +2,16 @@ package com.example.customview.ui.custom
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import androidx.annotation.NonNull
 import androidx.annotation.Nullable
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.customview.databinding.ItemSectionBinding
 
 class ItemSection @JvmOverloads constructor(
     @NonNull context: Context,
     @Nullable attrs: AttributeSet? = null,
     defStyleAttrs: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttrs) {
-
+    private val binding = ItemSectionBinding.inflate(LayoutInflater.from(context))
 }
